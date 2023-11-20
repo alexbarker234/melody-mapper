e21bfbaa0ae058acc589a7f9142d9741933ad0ca
alexbarker234 <alex.barker234@gmail.com>
2023-11-20 16:47:14 +0800

    better mobile display

--- src/app/explore/[artistId]/trackItem.tsx
+++ src/app/explore/[artistId]/trackItem.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from "next/image";
-import styles from "./page.module.scss";
+import styles from "./trackItem.module.scss";
 
 interface TrackItemProps {
     track: Track;
